b1c8e3d327951722282ca3a3754c7251dcef3015
Raiyan <raiyan.sharif.1234@gmail.com>
2019-06-08 01:54:00 +0600

    Buttons added

--- app/src/main/java/android/example/allstringmusicapp/MainActivity.java
+++ app/src/main/java/android/example/allstringmusicapp/MainActivity.java
@@ -2,6 +2,9 @@ package android.example.allstringmusicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button)findViewById(R.id.play_btn);
+        Button pauseButton = (Button)findViewById(R.id.pause_btn);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Play",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        pauseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Pause",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
